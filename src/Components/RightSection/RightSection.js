cafb45f6293d391967ddcea31d748bc241ac573a
Rajeev Kumar <thakurrajeev37@gmail.com>
2020-06-28 21:08:16 +0530

    added charts

--- src/Components/RightSection/RightSection.js
+++ src/Components/RightSection/RightSection.js
@@ -1,9 +1,59 @@
 import React from "react";
+import { Chart } from "react-google-charts";
 class RightSection extends React.Component {
     render() {
         return (
             <section className="r-content">
                 <h1>Home</h1>
+                <Chart
+                    width={'500px'}
+                    height={'300px'}
+                    chartType="PieChart"
+                    loader={<div>Loading Chart</div>}
+                    data={[
+                        ['Task', 'Hours per Day'],
+                        ['Work', 11],
+                        ['Eat', 2],
+                        ['Commute', 2],
+                        ['Watch TV', 2],
+                        ['Sleep', 7],
+                    ]}
+                    options={{
+                        title: 'My Daily Activities',
+                    }}
+                    rootProps={{ 'data-testid': '1' }}
+                />
+                <br></br>
+                <Chart
+                    width={'600px'}
+                    height={'400px'}
+                    chartType="LineChart"
+                    loader={<div>Loading Chart</div>}
+                    data={[
+                        ['x', 'dogs'],
+                        [0, 0],
+                        [1, 10],
+                        [2, 23],
+                        [3, 17],
+                        [4, 18],
+                        [5, 9],
+                        [6, 11],
+                        [7, 27],
+                        [8, 33],
+                        [9, 40],
+                        [10, 32],
+                        [11, 35],
+                    ]}
+                    options={{
+                        hAxis: {
+                        title: 'Time',
+                        },
+                        vAxis: {
+                        title: 'Popularity',
+                        },
+                    }}
+                    rootProps={{ 'data-testid': '1' }}
+                />
             </section>
         );
     }
